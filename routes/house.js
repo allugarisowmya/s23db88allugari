c7f017ec7aad051d1ad8526da95666bb22c1d9c8
Sowmya Allugari <allugarisowmya2001@gmail.com>
2023-11-15 18:40:52 -0600

    updated

--- routes/house.js
+++ routes/house.js
@@ -11,6 +11,9 @@ router.get('/detail', house_controlers.house_view_one_Page);
 router.get('/create', house_controlers.house_create_Page);
 /* GET create update page */
 router.get('/update', house_controlers.house_update_Page);
+/* GET delete house page */
+router.get('/delete', house_controlers.house_delete_Page);
+
 
 
 module.exports = router;
