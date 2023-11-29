2e549dac5f5966cd95e0cbbabb3ff95679141ead
Sowmya Allugari <allugarisowmya2001@gmail.com>
2023-11-29 01:44:53 -0600

    updated

--- routes/house.js
+++ routes/house.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const house_controlers= require('../controllers/house');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', house_controlers.house_view_all_Page );
@@ -10,7 +19,7 @@ router.get('/detail', house_controlers.house_view_one_Page);
 /* GET create house page */
 router.get('/create', house_controlers.house_create_Page);
 /* GET create update page */
-router.get('/update', house_controlers.house_update_Page);
+router.get('/update', secured, house_controlers.house_update_Page);
 /* GET delete house page */
 router.get('/delete', house_controlers.house_delete_Page);
 
