c6d0aa1287957fafa105432561451de4bfcce97c
Sowmya Allugari <allugarisowmya2001@gmail.com>
2023-11-29 02:10:34 -0600

    updated

--- models/house.js
+++ models/house.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose")
 const houseSchema = mongoose.Schema({
 houseName: String,
-price: Number,
-description: String
+description: String,
+price: { type: Number,
+    min: 100,
+    max: 300
+  }
 })
 module.exports = mongoose.model("house",
 houseSchema)
--- routes/house.js
+++ routes/house.js
@@ -15,13 +15,13 @@ res.redirect("/login");
 router.get('/', house_controlers.house_view_all_Page );
 
 /* GET detail house page */
-router.get('/detail', house_controlers.house_view_one_Page);
+router.get('/detail', secured, house_controlers.house_view_one_Page);
 /* GET create house page */
-router.get('/create', house_controlers.house_create_Page);
+router.get('/create', secured, house_controlers.house_create_Page);
 /* GET create update page */
 router.get('/update', secured, house_controlers.house_update_Page);
 /* GET delete house page */
-router.get('/delete', house_controlers.house_delete_Page);
+router.get('/delete', secured, house_controlers.house_delete_Page);
 
 
 
